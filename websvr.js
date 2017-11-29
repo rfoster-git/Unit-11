19bf69ef7e1138898fb54c6845a7ddcd88659790
rfoster-git <foster.ryan01@gmail.com>
2017-11-28 21:49:08 -0800

    Add files via upload

---
+++ websvr.js
@@ -0,0 +1,11 @@
+var http = require("http");
+
+http.createServer( function (request, response) {
+
+	response.writeHead(200, {'Content-Type': 'text/plain'});
+
+	response.end('Hello World, my name is Ryan Foster');
+
+}).listen(8001);
+
+console.log('Server running at http://127.0.0.1:8001/');
